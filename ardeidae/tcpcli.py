b644e6b030fec202f58011499d7691aecb7c05d8
Kai Weeks <weleoka@github.com>
2015-03-26 12:01:17 +0100

    work in progress.

--- ardeidae/tcpcli.py
+++ ardeidae/tcpcli.py
@@ -1,7 +1,6 @@
 import socket
 import sys
 
-
 # HOST, PORT = "sweet.student.bth.se", 8120                           #connect to bth, port
 # HOST, PORT = "seekers.student.bth.se", 8120                           #connect to bth, port
 # HOST, PORT = "192.168.1.36", 8120                           #connect to localhost, port
@@ -11,9 +10,73 @@ HOST, PORT = "localhost", 8120
 # HOST, PORT = "ardeidae.computersforpeace.net", 8120
 
 
-print (" ")
-print ("Started ardeidae_py TCP client.")
-print ("Trying to connect to TCP server: ", HOST, " on port: ", PORT, "...wait.")
+
+def printStartupMsg():
+    print (" ")
+    print ("Started ardeidae_py TCP client.")
+    print ("Trying to connect to TCP server: ", HOST, " on port: ", PORT, "...wait.")
+
+
+
+def startHere (theConnection):
+    message = input('PROMPT: ')
+
+    typedInteger = False
+    try:
+        typedInteger = int(message)
+    except:
+        pass
+
+    if len(message) > 0:
+        if str(message) == 'quit':
+            quitNow(theConnection)
+        else:
+            theConnection.sendall(message.encode('utf-8'))
+            received = theConnection.recv(1024)
+
+    else:
+        print ("Nothing sent. Please input a string or integer to transmit.")
+        received = "Nothing recieved because nothing sent."
+
+    if typedInteger:
+        dataRecieved = recv_file_with_size (theConnection, typedInteger)
+        outputFile(dataRecieved)
+        quitNow(theConnection)
+
+    elif hasattr(received, 'decode'):
+        print ("\n-> Sent: " + message )
+        print ("<- Received: " + received.decode('utf-8'))
+        quitNow(theConnection)
+
+
+
+def outputFile(dataStr):
+    print("Length of recieved data is: ")
+    print(len(dataStr))
+    print (dataStr.decode('utf-8'))
+
+
+
+def recv_file_with_size(cnct, size):
+    print("Size:", size)
+    msg = b''
+    while len(msg) < size:
+        print("msg length:", len(msg))
+        chunk = cnct.recv(size-len(msg))
+        if chunk == '':
+            raise RuntimeError("Socket connection broken")
+        msg = msg + chunk
+    return msg
+
+
+
+def quitNow (cnct):
+    cnct.close()
+    print ("...disconnected from ", HOST)
+    print ("Shutting down client...")
+    sys.exit()
+
+
 
 # Create a socket (SOCK_STREAM means a TCP socket), connect to server.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,31 +84,8 @@ try:
     sock.connect((HOST, PORT))
     print ("...connected.")
     print ("(please input string to echo, or integer to request file of certain number of rows)")
-finally:
-    print ("")
-   # sc.close()
-
-
-def startHere (sc):
-
-    while sc:
-        message = input('PROMPT: ')
-        # messageBytes = str.encode(message)
-        if len(message) > 0:
-            if str(message) == 'quit':
-                print ("...disconnected from ", HOST)
-                sc.close()
-                sys.exit()
-            else:
-                sc.sendall(message.encode('utf-8'))
-                received = sc.recv(1024)
-        else:
-            print ("Nothing sent. Please input a string or integer to transmit.")
-            received = "Nothing recieved because nothing sent."
-
-        if hasattr(received, 'decode'):
-            print ("\n-> Sent: " + message )
-            print ("<- Received: " + received.decode('utf-8'))
-
+except:
+    print ("Failed to connect to server.")
+    sys.exit()
 
 startHere(sock)
--- ardeidae/tcpser.py
+++ ardeidae/tcpser.py
@@ -1,39 +1,47 @@
 '''
 This program generates a random file of certain length and sends it to a client.
-
-for line in temp_file.readlines():
-    print line
-
-import time
-#use time.time() on Linux
-start = time.clock()
-for x in range(1000):
-    pass
-stop = time.clock()
-
-print stop-start
-
-start = time.clock()
-for x in xrange(1000):
-    pass
-stop = time.clock()
-
-print stop-start
-
+If anything other than an integer is recieved from client it will echo the string back.
+After processing one request the server will shut down.
 '''
 
-import socketserver
-import socket
-import datetime
-import tempfile
-import os
+import socketserver, socket, datetime, tempfile, os, sys
+
+
+
+def makeFile(ri):
+    tf = tempfile.NamedTemporaryFile()
+
+    for x in range(0, ri):
+        chunkStr = 'A'
+        chunk = chunkStr.encode('utf-8')
+        tf.write(chunk)
+
+    return tf
+
+
+
+def printStartupMsg():
+    print (" ")
+    print ("Started ardeidae_py TCP server.")
+    print("Started TCP Server... waiting for clients.")
+    print ("Server host name: ", socket.gethostname(), " on ", socket.gethostbyname(socket.gethostname()), " port: ", PORT)
+    print ("fully qualified domain name: ", socket.getfqdn())
+    print ("details: ", socket.gethostbyaddr(socket.gethostbyname(socket.gethostname())))
+
+
+
+def requestCompleted ():
+    print ("The servers job is done.")
+    print ("The TCP connection will be in Wait state, connect with a client again or ctrl + c to quit.")
+
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        recievedInteger = 0
+        recievedInteger = False
         filetosend = False
 
         if len(self.data) > 0:
@@ -43,36 +51,35 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             except:
                 pass
 
-            if recievedInteger > 0:
-                tf = tempfile.NamedTemporaryFile()
-                for x in range(1, recievedInteger + 1):
-                    if x < 10:
-                        chunkStr = 'line : 0' + str(x) + '\n'
-                    else:
-                        chunkStr = 'line : ' + str(x) + '\n'
+            if recievedInteger:
+                tempFile = makeFile(recievedInteger)
 
-                    chunk = chunkStr.encode('utf-8')
-                    tf.write(chunk)
+                # Read the information from the file.
+                tempFile.seek(0)
+                filetosend = tempFile.read()
+                metadata = os.stat(tempFile.name)
+                tempFile.close()
 
-                tf.seek(0)
-                filetosend = tf.read()
-                metadata = os.stat(tf.name)
-                tf.close()
+                # send confirmation message
+                # sendingConfirm = "Transfer of file: " + tempFile.name + " size: " + str(metadata.st_size) + " Bytes starting...\n"
+                # self.request.sendall(sendingConfirm.encode("utf-8"))
 
-                sendingConfirm = "Transfer of file: " + tf.name + " size: " + str(metadata.st_size) + " Bytes \n" #Message = "Sending " + filename + split + filesize
-
-                self.request.sendall(sendingConfirm.encode("utf-8"))  #send confirmation message
-                self.request.sendall(filetosend)                            #send file
-                print ("Sent a file to client of " + str(metadata.st_size) + " Bytes.")
+                #send file
+                print ("\nSending a file (", tempFile.name, ") to client of " + str(metadata.st_size) + " Bytes.")
+                self.request.sendall(filetosend)
+                requestCompleted()
 
             else:
                 timestamp = datetime.datetime.now().strftime("%I:%M%p")
-                print (timestamp, "{} wrote: ".format(self.client_address[0]))
+                print ("\n", timestamp, "{} wrote: ".format(self.client_address[0]))
                 print (self.data)
                 # just send back the same data, but upper-cased
                 self.request.sendall(self.data.upper())
+                requestCompleted()
+
         else:
-            print ("recieved client request of absolutely nothing")
+            print ("recieved client request of absolutely nothing.")
+            requestCompleted()
 
 
 if __name__ == "__main__":
@@ -85,12 +92,9 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True;
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
-    print (" ")
-    print ("Started ardeidae_py TCP server.")
-    print("Started TCP Server... waiting for clients.")
-    print ("Server host name: ", socket.gethostname(), " on ", socket.gethostbyname(socket.gethostname()), " port: ", PORT)
-    print ("fully qualified domain name: ", socket.getfqdn())
-    print ("details: ", socket.gethostbyaddr(socket.gethostbyname(socket.gethostname())))
+    printStartupMsg()
+
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
